28a0a45425d10cc74790d9e8f227ca2dda6960b6
matib83 <efbingenieria@gmail.com>
2021-09-13 10:54:43 -0300

    Pude hacer el ejercicio, pero hay que mejorar

--- Part1/courseinfo/src/index.js
+++ Part1/courseinfo/src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Header = ({course}) => <h1>{course}</h1>
+const Content = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+const Total = ({exercises}) => {
+  return (
+      <p>Number of exercises {exercises}</p>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total exercises={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
