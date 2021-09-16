8d863dae60a20d7125aadf137c2bdeb0d4b83841
matib83 <efbingenieria@gmail.com>
2021-09-16 10:48:05 -0300

    Renderizar mediante click y actualizar componente

--- Part1/courseinfo/src/index.js
+++ Part1/courseinfo/src/index.js
@@ -5,15 +5,11 @@ import { useState } from 'react';
 const rootElemnet = document.getElementById('root');
 
 const App = (props) => {
-  const [contadorValue, contadorUpdate] = useState(0)
+  const [contadorValue, updateContador] = useState(0)
 
   //const contador = useState(0)
   //const contadorValue = contador[0]
   //const contadorUpdate = contador [1]
-    
-  setInterval(() => {
-    contadorUpdate(contadorValue + 1)
-  }, 2000)
 
 console.log("render")
 
@@ -21,6 +17,14 @@ console.log("render")
       <div>  
         <p>El valor del conatdor es: </p>
         <h1>{contadorValue}</h1>
+        <button 
+          onClick={() => {
+            console.log("click");
+            updateContador(contadorValue + 1);
+          }}
+        >
+          Incrementar
+        </button>
       </div>
     )
 }
