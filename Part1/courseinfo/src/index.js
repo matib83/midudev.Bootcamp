50828b32d2b6ffd1380a27db5ac0b3c1656a73d5
matib83 <efbingenieria@gmail.com>
2021-09-16 12:32:01 -0300

    Utilizo componentes y paso valores

--- Part1/courseinfo/src/index.js
+++ Part1/courseinfo/src/index.js
@@ -4,29 +4,51 @@ import { useState } from 'react';
 
 const rootElemnet = document.getElementById('root');
 
+const Counter = ({contador}) => {
+  return <h1>{contador}</h1>
+}
+
 const App = (props) => {
   const [contadorValue, updateContador] = useState(0)
 
-  //const contador = useState(0)
+  //const contador = useState(0) 
   //const contadorValue = contador[0]
   //const contadorUpdate = contador [1]
 
 console.log("render")
 
+const handleclick = () => {
+  updateContador(contadorValue + 1);
+  console.log("click");
+}
+
+const handleClickReset = () => {
+  updateContador (0);
+}
+const isEven = contadorValue %2 === 0
+
+const mensajePar = isEven ? "Es par" : "Es impar"
+
   return (
       <div>  
         <p>El valor del conatdor es: </p>
-        <h1>{contadorValue}</h1>
+        <Counter contador={contadorValue}/>
+        <p>{mensajePar}</p>
         <button 
-          onClick={() => {
-            console.log("click");
-            //updateContador(contadorValue + 1);
-            updateContador(prevContador => {
-              return prevContador + 1
-            })  
-          }}
+          onClick={handleclick}
+            //updateContador(prevContador => {
+            //  return prevContador + 1
+            //})  
         >
           Incrementar
+        </button>
+        <button 
+          onClick={handleClickReset}
+            //updateContador(prevContador => {
+            //  return prevContador + 1
+            //})  
+        >
+          Reset
         </button>
       </div>
     )
