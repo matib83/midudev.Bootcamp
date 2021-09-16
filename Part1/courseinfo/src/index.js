4f9b18a02f328525d4d5d8ed982ccb5a73b21fca
matib83 <efbingenieria@gmail.com>
2021-09-16 10:09:24 -0300

    Hacemos renderizado por SetInterval (no se usa)

--- Part1/courseinfo/src/index.js
+++ Part1/courseinfo/src/index.js
@@ -1,23 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import { useState } from 'react';
+import { useState } from 'react';
 
 const rootElemnet = document.getElementById('root');
 
 const App = (props) => {
-return <h1>{props.contadorInicial}</h1>
-}
+  const [contadorValue, contadorUpdate] = useState(0)
 
-let contador = 0
+  //const contador = useState(0)
+  //const contadorValue = contador[0]
+  //const contadorUpdate = contador [1]
+    
+  setInterval(() => {
+    contadorUpdate(contadorValue + 1)
+  }, 2000)
 
-const refresh = () => {
-  ReactDOM.render(
-    <App contadorInicial={contador} />, 
-    rootElemnet
+console.log("render")
+
+  return (
+      <div>  
+        <p>El valor del conatdor es: </p>
+        <h1>{contadorValue}</h1>
+      </div>
     )
 }
 
-setInterval (() => { 
-  refresh();
-  contador++;
-}, 1000 )
+ReactDOM.render(<App />, rootElemnet)
+
