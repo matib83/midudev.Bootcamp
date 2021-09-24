aa1a96774ed489f5a732890bf32f49e836b7e3b8
Matias <efbingenieria@gmail.com>
2021-09-24 12:08:12 -0300

    Version final de mostrar el ganador (tengo 2 vectores diferentes)

--- Part1/anecdotas/src/index.js
+++ Part1/anecdotas/src/index.js
@@ -8,6 +8,24 @@ const Button = ({handleClick, text}) =>
     </button>
   </p>
 
+const MasVotada = ({points, anecdotes}) => {
+  const winningScore = Math.max(...points)
+  let winningIndex = undefined;
+  
+  points.forEach((point, index) => {
+    if(point===winningScore){
+      if(!winningIndex) winningIndex = index;
+    }
+  })
+
+  console.log({winningIndex})
+
+  console.log("Anecdota ganadra: ", winningIndex)
+  return ( 
+  <p>{anecdotes[winningIndex]}</p>
+  )
+}
+
 const App = (props) => {
   const maximo = 5, minimo = 0
   const [selected, setSelected] = useState(0)
@@ -17,19 +35,29 @@ const App = (props) => {
     const copyPints = [...points]
     copyPints[selected] +=1
     setPoints(copyPints)
+    console.log("Puntajes:", copyPints)
   }
 
   const handleClickSelected = () => setSelected(Math.round(Math.random() * ((maximo - minimo) + minimo)))
-  console.log(selected)
+  console.log("Anecdota elegida: ",selected)
 
   return (
     <div>
       {props.anecdotes[selected]}
       <Button handleClick={handleClickVote} text="Votar"/>
       <Button handleClick={handleClickSelected} text="Proxima anécdota"/>
+      <h1>Anecdota con mayoría de votos</h1>
+      <MasVotada points={points} anecdotes={anecdotes}/>
     </div>
   )
 }
+
+const data = [
+  {anecdota: 'hola', puntaje:0},
+  {anecdota: 'hola1', puntaje:0},
+  {anecdota: 'hola2', puntaje:0},
+  {anecdota: 'hola3', puntaje:0},
+]
 
 const anecdotes = [
   'If it hurts, do it more often',
