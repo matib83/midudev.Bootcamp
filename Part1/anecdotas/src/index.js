5de38817c37124fea61c6adb0f96dc9cd671125d
Matias <efbingenieria@gmail.com>
2021-10-27 12:08:46 -0300

    Queda el ejercicio funcionando sin errores

--- Part1/anecdotas/src/index.js
+++ Part1/anecdotas/src/index.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const anecdotes = [
+  {anecdota: 'If it hurts, do it more often', puntaje:0},
+  {anecdota: 'Adding manpower to a late software project makes it later!', puntaje:0},
+  {anecdota: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', puntaje:0 },
+  {anecdota: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', puntaje:0 },
+  {anecdota: 'Premature optimization is the root of all evil.', puntaje:0 },
+  {anecdota: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', puntaje:0}
+]
+
 const Button = ({handleClick, text}) => 
   <p>
     <button onClick={handleClick}>
@@ -9,17 +18,18 @@ const Button = ({handleClick, text}) =>
   </p>
 
 const MasVotada = ({anecdotes}) => {
-  const winningScore = Math.max(...anecdotes.puntaje)
-  let winningIndex = undefined;
+  const puntajes=anecdotes.map(anecdote=>anecdote.puntaje)
+  console.log({puntajes})
+  const winningScore = Math.max(...puntajes)
+  console.log({winningScore})
+  let winningIndex = 0;
 
   anecdotes.forEach((anecdote, index) => {
-    if(anecdotes.puntaje===winningScore){
+    if(anecdote.puntaje===winningScore){
       if(!winningIndex) winningIndex = index;
     }
   })
-
   console.log({winningIndex})
-
   console.log("Anecdota ganadra: ", anecdotes[winningIndex].anecdota)
   return ( 
   <p>{anecdotes[winningIndex].anecdota}</p>
@@ -27,20 +37,20 @@ const MasVotada = ({anecdotes}) => {
 }
 
 const App = (props) => {
-  const maximo = 5, minimo = 0
+  const MaxAnecdotasIndex = 5
   const [selected, setSelected] = useState(0)
   const [points, setPoints] = useState(anecdotes) //Asigno un estado como matriz
   
   const handleClickVote = () => {
-    const copyPints = [...points]
-    copyPints[selected].puntaje +=1
-    setPoints(copyPints)
-    console.log("Puntajes:", copyPints)
+    const copyPoints = [...points]
+    copyPoints[selected].puntaje +=1
+    setPoints(copyPoints)
+    console.log("Puntajes:", copyPoints)
   }
-
-  const handleClickSelected = () => setSelected(Math.round(Math.random() * ((maximo - minimo) + minimo)))
+  
+  const handleClickSelected = () => setSelected(Math.round(Math.random()*MaxAnecdotasIndex))
   console.log("Anecdota elegida: ",selected)
-
+  
   return (
     <div>
       {anecdotes[selected].anecdota}
@@ -51,15 +61,6 @@ const App = (props) => {
     </div>
   )
 }
-
-const anecdotes = [
-  {anecdota: 'If it hurts, do it more often', puntaje:0},
-  {anecdota: 'Adding manpower to a late software project makes it later!', puntaje:0},
-  {anecdota: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', puntaje:0 },
-  {anecdota: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', puntaje:0 },
-  {anecdota: 'Premature optimization is the root of all evil.', puntaje:0 },
-  {anecdota: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', puntaje:0}
-]
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
