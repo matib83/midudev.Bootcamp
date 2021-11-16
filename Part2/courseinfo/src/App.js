4f95c6fa3eb1a586bf898a4c67698a9404da90e0
Matias <efbingenieria@gmail.com>
2021-11-16 14:45:29 -0300

    Nuevo ejercicio, recibir objeto por props y crear Hook para añadir notas

--- Part2/courseinfo/src/App.js
+++ Part2/courseinfo/src/App.js
@@ -1,37 +1,26 @@
 import {Note} from './Note.js'
+import {useState} from 'react'
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true,
-  },
-]
+export default function App (props) {
 
-export default function App () {
-  if (typeof notes === "undefined" || notes.length === 0) {
-    return "No tenemos notas para mostrar"
+  const [notes, setNotes] = useState(props.notes)
+  //const [newNote, setNewNote] = useState('')
+
+  const handleChange = () => {
+    console.log('change')
   }
+
   return (
     <div>
       <h1>Notes</h1>
       <ol>
         {notes.map(note => <Note key={note.id} id={note.id} content={note.content} date={note.date} />)}
       </ol>
-    </div>
+      <div>
+        <input type='text' onChange={handleChange}/>
+        <button>Creat nota</button>
+      </div>
+    </div> 
   );
 }
 
