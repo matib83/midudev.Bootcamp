619816fa19514f1662c7b9efc187304caf2cd0ce
Matias <efbingenieria@gmail.com>
2021-11-17 10:59:54 -0300

    Agregamos un boton para mostrar solo notas importantes

--- Part2/courseinfo/src/App.js
+++ Part2/courseinfo/src/App.js
@@ -5,6 +5,7 @@ export default function App (props) {
 
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   const handleChange = (event) => {
     setNewNote(event.target.value)
@@ -27,11 +28,21 @@ export default function App (props) {
     setNewNote("")
   }
 
+  const handleShowAll = () => {
+    setShowAll(() => !showAll)
+  }
+
   return (
     <div> 
       <h1>Notes</h1>
+      <button onClick={handleShowAll}>{showAll ? "Show only important" : "Show all"}</button>
       <ol>
-        {notes.map(note => <Note key={note.id} id={note.id} content={note.content} date={note.date} />)}
+        {notes
+        .filter(note => {
+          if (showAll === true) return true // Si el estado showAll es true muestro TODAS las notas
+          return note.important === true    // Si no, solo muestro als notas IMPORTANTES
+        })
+        .map(note => <Note key={note.id} id={note.id} content={note.content} date={note.date} />)}
       </ol>
       <form onSubmit={handleSubmit}>
         <input type='text' onChange={handleChange} value={newNote}/>
