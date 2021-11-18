0eef2dd5ac2a2404072e754e2175c08e029e716a
Matias <efbingenieria@gmail.com>
2021-11-18 11:53:29 -0300

    Sumando con una variable (no se recomienda)

--- Part2/course/src/Course.js
+++ Part2/course/src/Course.js
@@ -1,11 +1,16 @@
+let acum = 0
 
 export const Course = ({name, parts}) => {
     console.log("Renderizando el curso")
     console.log({parts})
+    acum = 0
+    parts.map(part => acum+=part.exercises)
+    console.log(acum)
     return (
         <div>
             <h1>{name}</h1>
             {parts.map(part => <p key={part.id}> {part.name} {part.exercises} </p> )}
+            <b>Total of {acum} exercises </b>
         </div>
     )
 }
