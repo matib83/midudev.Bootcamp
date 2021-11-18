b8600805b0f9506a67c7997e2d9aa97a7494f7d7
Matias <efbingenieria@gmail.com>
2021-11-18 11:26:25 -0300

    Hogo renderizado del objeto para solo pasar lo que necesito al componente

--- Part2/course/src/App.js
+++ Part2/course/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
 
   return (
     <div>
-      {<Course course={course} />} 
+      {<Course {...course} />} 
     </div>)
 }
 
--- Part2/course/src/Course.js
+++ Part2/course/src/Course.js
@@ -1,11 +1,11 @@
 
-export const Course = ({course}) => {
+export const Course = ({name, parts}) => {
     console.log("Renderizando el curso")
-    console.log({course})
+    console.log({parts})
     return (
         <div>
-            <h1>{course.name}</h1>
-            {course.parts.map(part => <p key={part.id}> {part.name} {part.exercises} </p> )}
+            <h1>{name}</h1>
+            {parts.map(part => <p key={part.id}> {part.name} {part.exercises} </p> )}
         </div>
     )
 }
