c600d7f655cdd795b3fafaa912a80f15cc14f76f
Matias <efbingenieria@gmail.com>
2021-12-13 11:48:25 -0300

    Completamos paso uno mostrando todo lo q pide el ejercicio

--- Part2/paises/src/App.js
+++ Part2/paises/src/App.js
@@ -7,16 +7,18 @@ const WarningCountries = () => {
   return <>Muchos países para mostrar, agregue mas letras a la búsqueda</>
 }
 
-const Detalle = ({name, capital, population, languages}) => {
-
+const Detalle = ({name, capital, population, languages, flags}) => {
+  const idiomas = Object.values(languages)
+  console.log(idiomas)
   return (
     <div>
       <h1>{name.common}</h1>
       Capital: {capital}<br/>
       population: {population}
       <h3>Languages</h3>
-      <li>{Object.values(languages).forEach(element => console.log(element))}</li>
-      {Object.values(languages).forEach(element => console.log(element))}
+      {Object.values(languages).map(element => <li key={element}> {element}</li>)}
+      <br/>
+      <img src={flags.png} alt="BANDERA NO DISPONIBLE" style={{height: 100, width: 100}}/>
     </div>
   )
 }
