5b180300ab07ba14059a41007c7e090f30b18150
Matias <efbingenieria@gmail.com>
2022-01-28 12:11:18 -0300

    Corrijo deletePerson, falta actualizar la Bd sin refrescar

--- Part2/agenda/src/services/persons/deletePerson.js
+++ Part2/agenda/src/services/persons/deletePerson.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-export const deletePerson = ({userId}) => {
+export const deletePerson = (userId) => {
     console.log(`ID recibido para eliminar: ${userId} `)
     return axios
-    .delete('http://localhost:3001/persons/', {userId})
+    .delete(`http://localhost:3001/persons/${userId}`)
     .then(response =>{
       const {data} = response
       console.log(`${userId} ha sido eliminado de la base de datos`)
