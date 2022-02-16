b6f18da7f821acd5276c634ac69a48fa65e7f869
Matias <efbingenieria@gmail.com>
2022-02-16 11:52:01 -0300

    Mi servidor ante una peticion me devuelve la lista tipo json al navegador

--- Part3/index.js
+++ Part3/index.js
@@ -1,7 +1,29 @@
-const http = require('http')                                //Importar el modulo http utilizando Common.JS
+const http = require('http')    //Importar el modulo http utilizando Common.JS
+
+let notes = [
+    {
+      id: 1,
+      content: 'HTML is easy',
+      date: '2019-05-30T17:30:31.098Z',
+      important: true,
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      date: '2019-05-30T18:39:34.091Z',
+      important: false,
+    },
+    {
+      id: 3,
+      content: 'GET and POST are the most important methods of HTTP protocol',
+      date: '2019-05-30T19:20:14.298Z',
+      important: true,
+    },
+  ]
+
 const app = http.createServer((request, response) => {      //Callback, funcion que se ejecuta cada vez que
-    response.writeHead(200, { 'Content-Type':'text/plain'}) //le llegue un request (petición al servidor)
-    response.end('Hello Word')                              //responde enla cabecera con status 200
+    response.writeHead(200, { 'Content-Type':'application/json'}) //le llegue un request (petición al servidor)
+    response.end(JSON.stringify(notes))                              //responde enla cabecera con status 200
 })
 
 const PORT = 3001                                           //puerto por donde escucha mi servidor
